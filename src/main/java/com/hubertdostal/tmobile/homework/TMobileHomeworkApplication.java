00594fa873bff7d6dff678330b52d66c22d44232
hubert.dostal <hubert.dostal@gmail.com>
2022-09-25 12:53:41 +0200

    initial commit

---
+++ src/main/java/com/hubertdostal/tmobile/homework/TMobileHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.hubertdostal.tmobile.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TMobileHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TMobileHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO SCHEMA1.USERS (ID_, LOGIN_NAME_, FIRST_NAME_, LAST_NAME_) VALUES (1, 'hubert.dostal', 'Hubert', 'Dostal');
+INSERT INTO SCHEMA1.USERS (ID_, LOGIN_NAME_, FIRST_NAME_, LAST_NAME_) VALUES (2, 'john.doe', 'John', 'Doe');
+INSERT INTO SCHEMA1.USERS (ID_, LOGIN_NAME_, FIRST_NAME_, LAST_NAME_) VALUES (3, 'jane.mcdonald', 'Jane', 'McDonald');
+
+INSERT INTO SCHEMA2.TASKS (ID_, USER_NOTE_, TASK_DATA_, ACQUIRED_BY_, CREATED_BY_) VALUES (1, 'REST cotroller to be created using Spring Boot', 'Create sample REST controller serving simple task database', 2, 1);
+INSERT INTO SCHEMA2.TASKS (ID_, USER_NOTE_, TASK_DATA_, ACQUIRED_BY_, CREATED_BY_) VALUES (2, 'Write documentation to REST controller once done', 'Write documentation to REST controller once done and place it to Sharepoint', 3, 1);
